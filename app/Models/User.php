683f0a00156df73d390bf45df0e16801c95c7347
KomiljonovDev <uzokdeveloper@gmail.com>
2022-12-21 01:45:27 +0500

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
+    protected $guarded = [];
 
     /**
      * The attributes that are mass assignable.
