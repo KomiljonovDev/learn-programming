09db4553027683e4555689b0edc0493b9aeaa6ae
KomiljonovDev <uzokdeveloper@gmail.com>
2022-12-20 09:36:44 +0500

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,16 +6,18 @@
 	<title>PHP - komiljonovdev.uz</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script type="text/javascript" src="../js/prism.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/prism.css">
 	<style type="text/css">
 		*{
 			margin: 0;
 			padding: 0;
 			box-sizing: border-box;
 		}
-		.mode{
+		/*.mode{
 			background-color: #232529;
 			color: white;
-		}
+		}*/
 		.navbar-home{
 			font-size: 27px;
 			text-transform: uppercase;
